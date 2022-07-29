e182862decaca762d5b73e16f176eaf58dcfe922
GudymaVit <gudymavit1988@gmail.com>
2022-07-29 19:21:42 +0300

    Update index.jsx

--- src/components/moviedetails/index.jsx
+++ src/components/moviedetails/index.jsx
@@ -41,10 +41,14 @@ const MovieDetails = () => {
         <p>Additional information</p>
         <ul>
           <li>
-            <Link to="cast">Cast</Link>
+            <Link to="cast" state={{ from: location?.state?.from ?? '/' }}>
+              Cast
+            </Link>
           </li>
           <li>
-            <Link to="reviews">Reviews</Link>
+            <Link to="reviews" state={{ from: location?.state?.from ?? '/' }}>
+              Reviews
+            </Link>
           </li>
         </ul>
         <Suspense fallback={<div>Loading...</div>}>
